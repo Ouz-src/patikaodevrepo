3374e5074f526d05977b3f830450da76fcffe8f1
Uğur Oğuzhan Karadeniz <77590835+Ouz-src@users.noreply.github.com>
2022-01-21 17:52:24 +0300

    Add files via upload

---
+++ Csharp/AlgortihmQuestions/Odev4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csharprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle yazın.");
+            string cumle = Console.ReadLine();
+            int kelimeSayisi, harfSayisi;
+
+            string[] kelimeler = cumle.Split(" ");
+            kelimeSayisi = kelimeler.Length;
+            Console.WriteLine("Bu cümlede toplam {0} tane kelime var.",kelimeSayisi);
+            char[] harfler = string.Join("",kelimeler).ToCharArray();
+            harfSayisi = harfler.Length;
+            Console.WriteLine("Bu cümlede toplam {0} tane harf var.", harfSayisi);
+            Console.ReadKey();
+        }
+    }
+}
