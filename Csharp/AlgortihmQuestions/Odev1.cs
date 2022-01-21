abb0e4afd122753f8befc168116f1e4a7c221e53
Uğur Oğuzhan Karadeniz <77590835+Ouz-src@users.noreply.github.com>
2022-01-21 16:41:18 +0300

    Add files via upload

---
+++ Csharp/AlgortihmQuestions/Odev1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Csharprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eleman sayısını girin: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] dizi = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}. sayıyı girin: ", i + 1);
+                dizi[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Çift sayılar:");
+            foreach (var sayi in dizi)
+            {
+                if (sayi % 2 == 0)
+                    Console.Write(" " + sayi);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
