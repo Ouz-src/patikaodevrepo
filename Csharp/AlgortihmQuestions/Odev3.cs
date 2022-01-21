c2bb63d6136bcb252c2247b6efe2a39d10138722
Uğur Oğuzhan Karadeniz <77590835+Ouz-src@users.noreply.github.com>
2022-01-21 17:34:31 +0300

    Add files via upload

---
+++ Csharp/AlgortihmQuestions/Odev3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Csharprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı girin (n): ");
+            int n = int.Parse(Console.ReadLine());
+            string[] kelimeler = new string[n];
+            Console.WriteLine("{0} tane kelime girin:",n);
+            for (int i = 0; i < n; i++)
+            {
+                kelimeler[i] = Console.ReadLine();
+            }
+
+            Array.Reverse(kelimeler);
+            foreach (var kelime in kelimeler)
+            {
+                Console.Write(kelime + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
