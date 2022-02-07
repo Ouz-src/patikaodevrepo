0917e70348c9a365f25401c32e17a4d14507869c
Uğur Oğuzhan Karadeniz <77590835+Ouz-src@users.noreply.github.com>
2022-02-07 18:04:02 +0300

    Add files via upload

---
+++ Java/DiamondwithStars.java
@@ -0,0 +1,31 @@
+package TestingPack;
+
+import java.util.Scanner;
+
+public class DiamondwithStars {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter step count: ");
+        int n = input.nextInt();
+        n = (n/2);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 2*i-1; k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+}
