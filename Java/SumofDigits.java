14a1fbd9382e229e4d55b4d9f2d74167d4c5d77e
Uğur Oğuzhan Karadeniz <77590835+Ouz-src@users.noreply.github.com>
2022-02-07 17:31:47 +0300

    Update SumofDigits.java

--- Java/SumofDigits.java
+++ Java/SumofDigits.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class PowerCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
         int n = input.nextInt();
         short sum = 0;
         while (n % 10 > 0) {
             sum += n%10; 
             n/= 10;
         }
-        System.out.println(sum);
+        System.out.println("Sum of digits: " + sum);
         input.close();
     }
 }
